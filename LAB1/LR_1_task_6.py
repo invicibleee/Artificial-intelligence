ed0ac55ba2e79f0ca1bb29a564179316100af601
Даніла Кірій <kn201_kdo@student.ztu.edu.ua>
2023-11-15 18:36:51 +0200

    LAB 1 update

---
+++ LAB1/LR_1_task_6.py
@@ -0,0 +1,34 @@
+import matplotlib
+import numpy as np
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.svm import SVC
+from utilities import visualize_classifier
+
+matplotlib.use('TkAgg')
+
+input_file = 'data_multivar_nb.txt'
+
+data = np.loadtxt(input_file, delimiter=',')
+X, y = data[:, :-1], data[:, -1]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=3)
+
+classifier = SVC()
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X)
+y_test_pred = classifier.predict(X_test)
+
+accuracy = 100.0 * (y_test == y_test_pred).sum() / X_test.shape[0]
+print("Accuracy of SVC classifier =", round(accuracy, 2), "%")
+
+num_folds = 3
+accuracy_values = cross_val_score(classifier, X, y, scoring='accuracy', cv=num_folds)
+print("Accuracy: " + str(round(100 * accuracy_values.mean(), 2)) + "%")
+precision_values = cross_val_score(classifier, X, y, scoring='precision_weighted', cv=num_folds)
+print("Precision: " + str(round(100 * precision_values.mean(), 2)) + "%")
+recall_values = cross_val_score(classifier, X, y, scoring='recall_weighted', cv=num_folds)
+print("Recall: " + str(round(100 * recall_values.mean(), 2)) + "%")
+f1_values = cross_val_score(classifier, X, y, scoring='f1_weighted', cv=num_folds)
+print("F1: " + str(round(100 * f1_values.mean(), 2)) + "%")
+
+visualize_classifier(classifier, X_test, y_test)
